04f91755050d1a588def9af83fe21d29f3e81250
Serkan Sahin <ssahin55@outlook.com>
2020-08-10 06:00:03 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,7 @@
 current_time = Time.now
-if current_time.to_i % 2.0 == 0
-  return "Even!"
+current_time = current_time.to_i
+if current_time % 2 == 0 
+  puts "Even!"
 else
-  return "Odd!"
+  puts "Odd!"
 end
